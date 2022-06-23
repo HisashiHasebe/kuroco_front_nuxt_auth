f5713bb844971fce3d9f883800442d1d867fc250
HisashiHasebe <hhasebe@diverta.co.jp>
2022-06-23 16:52:25 +0800

    開発環境でプッシュ

--- env.development.js
+++ env.development.js
@@ -1,5 +1,5 @@
 module.exports = {
-    META_TITLE: '[開発]Nuxt Auth',
+    META_TITLE: '[開発dev]Nuxt Auth',
     ROBOTS: 'noindex',
     BASE_URL: 'https://hasebetest-front-nuxt-auth.g.kuroco.app'
 };
